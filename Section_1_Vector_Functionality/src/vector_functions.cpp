df1abf40168ff18b25c08433a1e4a041e3fcb3b6
August <augustjaubert@gmail.com>
2024-01-19 14:47:47 +0000

    fixed & completed the vector functionality exercise

--- Section_1_Vector_Functionality/src/vector_functions.cpp
+++ Section_1_Vector_Functionality/src/vector_functions.cpp
@@ -3,16 +3,25 @@
 #include <iostream>
 
 // write your print vector function here
-
-int countMultiplesOfFive(/*take a vector of ints here*/)
+int countMultiplesOfFive(std::vector<int> inputVector)
 {
+    int temp = 0;
+    int counter = 0;
 
+    for (int i = 0; i < inputVector.size(); i++)
+    {
+        temp = inputVector[i];
+        if (temp % 5 == 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
 }
-
 
 void addElements(std::vector<int> v, int x, int n)
 {
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         v.push_back(x);
     }
