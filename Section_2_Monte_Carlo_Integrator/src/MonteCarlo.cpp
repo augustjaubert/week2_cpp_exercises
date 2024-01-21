b6a88d1a3e0d19cffafa6ebd47a616ddc29084af
August <augustjaubert@gmail.com>
2024-01-21 14:17:36 +0000

    Finished Monte Carlo exercise.

--- Section_2_Monte_Carlo_Integrator/src/MonteCarlo.cpp
+++ Section_2_Monte_Carlo_Integrator/src/MonteCarlo.cpp
@@ -4,10 +4,10 @@
 
 bool InsideUnitSphere(double x, double y, double z)
 {
-    return ((x*x + y*y + z*z) <= 1);
+    return ((x * x + y * y + z * z) <= 1);
 }
 
-double IntegrateMonteCarlo3D(int n_points, double min, double max)
+double IntegrateMonteCarlo3D(int n_points, double min, double max, std::function<bool(double, double, double)> TestPoint)
 {
     int count = 0;
     // N.B. std::pow with integer arguments can be less efficient than
@@ -15,26 +15,33 @@ double IntegrateMonteCarlo3D(int n_points, double min, double max)
     // inside loops for example!
     double VolCube = std::pow((max - min), 3);
 
-    //set up random number generator here
+    // set up random number generator here
+    std::mt19937 gen(567); // For most research applications we want to set the seed explicitly, as this makes the results reproducible.
+    // std::default_random_engine gen; // Another random number engine, no seed explicitly set.
+    std::uniform_real_distribution<double> uniform_dist(min, max);
+    auto genRandDouble = std::bind(uniform_dist, gen);
 
-    for(int i = 0; i < n_points; i++)
+    for (int i = 0; i < n_points; i++)
     {
-        //generate random points here
-        double x = 
-        double y =
-        double z = 
-        if(InsideUnitSphere(x, y, z)) count++;
+        // generate random points here
+        auto x = genRandDouble();
+        //std::cout << i << " , " << x << std::endl;
+        auto y = genRandDouble();
+        auto z = genRandDouble();
+        if (TestPoint(x, y, z))
+            count++;
     }
 
     return static_cast<double>(count) / n_points * VolCube;
 }
 
-
 int main()
 {
     int N_points = 10000;
-    
-    double UnitSphereVol = IntegrateMonteCarlo3D(N_points, -1.0, 1.0);
+
+    double UnitSphereVol = IntegrateMonteCarlo3D(N_points, -1.0, 1.0, InsideUnitSphere);
 
     std::cout << "Volume estimate of sphere using " << N_points << " points = " << UnitSphereVol << std::endl;
+
+    return 0;
 }
